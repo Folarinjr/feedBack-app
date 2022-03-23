a4051090be8f6c11d58736a1c6a9ee265fb780bf
Folarinjr <folarinademola2@gmail.com>
2022-03-23 19:24:15 +0100

    Bug fixed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 import Header from './components/Header';
 import FeedBackList from './components/FeedBackList';
-
 import FeedBackStats from './components/FeedBackStats';
 import FeedBackForm from './components/FeedBackForm';
 
